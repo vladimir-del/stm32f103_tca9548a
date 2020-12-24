0b0254773002364c5d0a8ddeea8a3d3522c541b4
vladimir-del <56100237+vladimir-del@users.noreply.github.com>
2020-12-24 14:48:10 +1000

    Update tca9548a.c
    Created new function Tca9548ResetPOR for Power On Reset. 
    In Tca9548ResetInput() added a StatusReg value to check the status register after a reset.

--- Core/Src/tca9548a.c
+++ Core/Src/tca9548a.c
@@ -7,19 +7,46 @@
 
 #include "tca9548a.h"
 #include "i2c.h"
+#include "gpio.h"
 
-HAL_StatusTypeDef SelectCh(uint8_t channel)
+HAL_StatusTypeDef Tca9548SelectCh(uint8_t channel)
 {
-	if (HAL_I2C_Mem_Write(&hi2c1, TCA9548_ADDR << 1, channel, I2C_MEMADD_SIZE_8BIT, (void*)0, 0, 100) != HAL_OK)
+	uint8_t StatusReg = 0;
+	HAL_I2C_Mem_Write(&hi2c1, TCA9548_ADDR << 1, channel, I2C_MEMADD_SIZE_8BIT, (void*)0, 0, 100)
+	
+	HAL_I2C_Mem_Read(&hi2c1, TCA9548_ADDR << 1, channel, I2C_MEMADD_SIZE_8BIT, StatusReg, 0, 100)
+	
+	if (StatusReg == channel)
 		return HAL_ERROR;
-	else
+	else 
 		return HAL_OK;
+	
 }
 
 
-void Reset_TCA9548(void)
+HAL_StatusTypeDef Tca9548ResetInput(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin)
 {
-	HAL_GPIO_TogglePin(Reset_TCA9548_GPIO_Port, Reset_TCA9548_Pin);
+	uint8_t StatusReg = 0;
+	HAL_GPIO_TogglePin(GPIOx, GPIO_Pin);
 	HAL_Delay(10);
-	HAL_GPIO_TogglePin(Reset_TCA9548_GPIO_Port, Reset_TCA9548_Pin);
+	HAL_GPIO_TogglePin(GPIOx, GPIO_Pin);
+	
+	HAL_I2C_Mem_Read(&hi2c1, TCA9548_ADDR << 1, channel, I2C_MEMADD_SIZE_8BIT, StatusReg, 0, 100)
+	if (StatusReg == 0x0)
+		return HAL_OK;
+	else
+		return HAL_ERROR;
+}
+
+HAL_StatusTypeDef Tca9548ResetPOR(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin)
+{
+	HAL_GPIO_TogglePin(GPIOx, GPIO_Pin);
+	HAL_Delay(10);
+	HAL_GPIO_TogglePin(GPIOx, GPIO_Pin);
+	
+	HAL_I2C_Mem_Read(&hi2c1, TCA9548_ADDR << 1, channel, I2C_MEMADD_SIZE_8BIT, StatusReg, 0, 100)
+	if (StatusReg == 0x0)
+		return HAL_OK;
+	else
+		return HAL_ERROR;
 }
